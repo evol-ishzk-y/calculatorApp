b9bdcc47fd0de50d8c3c56578afe0a41c0dc7f5c
evol-ishzk-y <ishizuka-y@evoltech.co.jp>
2026-01-27 14:05:54 +0900

    プロジェクト ファイルを追加します。

---
+++ calculatorApp/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace calculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            seven = new Button();
+            eight = new Button();
+            nine = new Button();
+            four = new Button();
+            five = new Button();
+            six = new Button();
+            one = new Button();
+            two = new Button();
+            three = new Button();
+            zero = new Button();
+            dot = new Button();
+            all_clear = new Button();
+            sign = new Button();
+            persent = new Button();
+            divided = new Button();
+            equal = new Button();
+            plus = new Button();
+            minus = new Button();
+            multiplied = new Button();
+            Num_Label = new Label();
+            mark = new Label();
+            SuspendLayout();
+            // 
+            // seven
+            // 
+            seven.Font = new Font("Yu Gothic UI", 15F);
+            seven.Location = new Point(63, 469);
+            seven.Name = "seven";
+            seven.Size = new Size(188, 174);
+            seven.TabIndex = 0;
+            seven.Text = "7";
+            seven.UseVisualStyleBackColor = true;
+            // 
+            // eight
+            // 
+            eight.Font = new Font("Yu Gothic UI", 15F);
+            eight.Location = new Point(275, 469);
+            eight.Name = "eight";
+            eight.Size = new Size(188, 174);
+            eight.TabIndex = 1;
+            eight.Text = "8";
+            eight.UseVisualStyleBackColor = true;
+            // 
+            // nine
+            // 
+            nine.Font = new Font("Yu Gothic UI", 15F);
+            nine.Location = new Point(491, 469);
+            nine.Name = "nine";
+            nine.Size = new Size(188, 174);
+            nine.TabIndex = 2;
+            nine.Text = "9";
+            nine.UseVisualStyleBackColor = true;
+            // 
+            // four
+            // 
+            four.Font = new Font("Yu Gothic UI", 15F);
+            four.Location = new Point(63, 672);
+            four.Name = "four";
+            four.Size = new Size(188, 174);
+            four.TabIndex = 3;
+            four.Text = "4";
+            four.UseVisualStyleBackColor = true;
+            // 
+            // five
+            // 
+            five.Font = new Font("Yu Gothic UI", 15F);
+            five.Location = new Point(275, 672);
+            five.Name = "five";
+            five.Size = new Size(188, 174);
+            five.TabIndex = 4;
+            five.Text = "5";
+            five.UseVisualStyleBackColor = true;
+            // 
+            // six
+            // 
+            six.Font = new Font("Yu Gothic UI", 15F);
+            six.Location = new Point(491, 672);
+            six.Name = "six";
+            six.Size = new Size(188, 174);
+            six.TabIndex = 5;
+            six.Text = "6";
+            six.UseVisualStyleBackColor = true;
+            // 
+            // one
+            // 
+            one.Font = new Font("Yu Gothic UI", 15F);
+            one.Location = new Point(63, 873);
+            one.Name = "one";
+            one.Size = new Size(188, 174);
+            one.TabIndex = 6;
+            one.Text = "1";
+            one.UseVisualStyleBackColor = true;
+            // 
+            // two
+            // 
+            two.Font = new Font("Yu Gothic UI", 15F);
+            two.Location = new Point(275, 873);
+            two.Name = "two";
+            two.Size = new Size(188, 174);
+            two.TabIndex = 7;
+            two.Text = "2";
+            two.UseVisualStyleBackColor = true;
+            // 
+            // three
+            // 
+            three.Font = new Font("Yu Gothic UI", 15F);
+            three.Location = new Point(491, 873);
+            three.Name = "three";
+            three.Size = new Size(188, 174);
+            three.TabIndex = 8;
+            three.Text = "3";
+            three.UseVisualStyleBackColor = true;
+            // 
+            // zero
+            // 
+            zero.Font = new Font("Yu Gothic UI", 15F);
+            zero.Location = new Point(63, 1082);
+            zero.Name = "zero";
+            zero.Size = new Size(400, 174);
+            zero.TabIndex = 9;
+            zero.Text = "0";
+            zero.UseVisualStyleBackColor = true;
+            // 
+            // dot
+            // 
+            dot.Font = new Font("Yu Gothic UI", 15F);
+            dot.Location = new Point(491, 1082);
+            dot.Name = "dot";
+            dot.Size = new Size(188, 174);
+            dot.TabIndex = 10;
+            dot.Text = ".";
+            dot.UseVisualStyleBackColor = true;
+            // 
+            // all_clear
+            // 
+            all_clear.Font = new Font("Yu Gothic UI", 15F);
+            all_clear.Location = new Point(63, 265);
+            all_clear.Name = "all_clear";
+            all_clear.Size = new Size(188, 174);
+            all_clear.TabIndex = 11;
+            all_clear.Text = "A/C";
+            all_clear.UseVisualStyleBackColor = true;
+            // 
+            // sign
+            // 
+            sign.Font = new Font("Yu Gothic UI", 15F);
+            sign.Location = new Point(275, 265);
+            sign.Name = "sign";
+            sign.Size = new Size(188, 174);
+            sign.TabIndex = 12;
+            sign.Text = "＋/ー";
+            sign.UseVisualStyleBackColor = true;
+            // 
+            // persent
+            // 
+            persent.Font = new Font("Yu Gothic UI", 15F);
+            persent.Location = new Point(491, 265);
+            persent.Name = "persent";
+            persent.Size = new Size(188, 174);
+            persent.TabIndex = 13;
+            persent.Text = "%";
+            persent.UseVisualStyleBackColor = true;
+            // 
+            // divided
+            // 
+            divided.Font = new Font("Yu Gothic UI", 15F);
+            divided.Location = new Point(712, 265);
+            divided.Name = "divided";
+            divided.Size = new Size(188, 174);
+            divided.TabIndex = 18;
+            divided.Text = "÷";
+            divided.UseVisualStyleBackColor = true;
+            // 
+            // equal
+            // 
+            equal.Font = new Font("Yu Gothic UI", 15F);
+            equal.Location = new Point(712, 1082);
+            equal.Name = "equal";
+            equal.Size = new Size(188, 174);
+            equal.TabIndex = 17;
+            equal.Text = "=";
+            equal.UseVisualStyleBackColor = true;
+            // 
+            // plus
+            // 
+            plus.Font = new Font("Yu Gothic UI", 15F);
+            plus.Location = new Point(712, 873);
+            plus.Name = "plus";
+            plus.Size = new Size(188, 174);
+            plus.TabIndex = 16;
+            plus.Text = "+";
+            plus.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            minus.Font = new Font("Yu Gothic UI", 15F);
+            minus.Location = new Point(712, 672);
+            minus.Name = "minus";
+            minus.Size = new Size(188, 174);
+            minus.TabIndex = 15;
+            minus.Text = "-";
+            minus.UseVisualStyleBackColor = true;
+            // 
+            // multiplied
+            // 
+            multiplied.Font = new Font("Yu Gothic UI", 15F);
+            multiplied.Location = new Point(712, 469);
+            multiplied.Name = "multiplied";
+            multiplied.Size = new Size(188, 174);
+            multiplied.TabIndex = 14;
+            multiplied.Text = "×";
+            multiplied.UseVisualStyleBackColor = true;
+            // 
+            // Num_Label
+            // 
+            Num_Label.AutoSize = true;
+            Num_Label.Font = new Font("Yu Gothic UI", 25F);
+            Num_Label.Location = new Point(789, 86);
+            Num_Label.Name = "Num_Label";
+            Num_Label.Size = new Size(111, 133);
+            Num_Label.TabIndex = 19;
+            Num_Label.Text = "0";
+            // 
+            // mark
+            // 
+            mark.AutoSize = true;
+            mark.Font = new Font("Yu Gothic UI", 25F);
+            mark.Location = new Point(78, 86);
+            mark.Name = "mark";
+            mark.Size = new Size(125, 133);
+            mark.TabIndex = 20;
+            mark.Text = "=";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(969, 1309);
+            Controls.Add(mark);
+            Controls.Add(Num_Label);
+            Controls.Add(divided);
+            Controls.Add(equal);
+            Controls.Add(plus);
+            Controls.Add(minus);
+            Controls.Add(multiplied);
+            Controls.Add(persent);
+            Controls.Add(sign);
+            Controls.Add(all_clear);
+            Controls.Add(dot);
+            Controls.Add(zero);
+            Controls.Add(three);
+            Controls.Add(two);
+            Controls.Add(one);
+            Controls.Add(six);
+            Controls.Add(five);
+            Controls.Add(four);
+            Controls.Add(nine);
+            Controls.Add(eight);
+            Controls.Add(seven);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button seven;
+        private Button eight;
+        private Button nine;
+        private Button four;
+        private Button five;
+        private Button six;
+        private Button one;
+        private Button two;
+        private Button three;
+        private Button zero;
+        private Button dot;
+        private Button all_clear;
+        private Button sign;
+        private Button persent;
+        private Button divided;
+        private Button equal;
+        private Button plus;
+        private Button minus;
+        private Button multiplied;
+        private Label Num_Label;
+        private Label mark;
+    }
+}
