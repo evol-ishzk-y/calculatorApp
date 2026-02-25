d82a129f594c6385df551fb68bc71ed05c333ca7
evol-ishzk-y <ishizuka-y@evoltech.co.jp>
2026-02-25 13:42:54 +0900

    Login画面実装

---
+++ calculatorApp/DatabaseHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace calculatorApp
+{
+    internal class DatabaseHelper
+    {
+        private string connString = "Host=localhost; Port=5432; Username=postgres; Password=password123; Database=my_calculator_db";
+    }
+}
